a8daa97ed3cca68738be865f1dba1d7d9b0ad654
taitq2 <taitq2@vng.com.vn>
2026-06-22 17:17:26 +0700

    Initial commit

---
+++ Fresher2026/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Fresher2026
+//
+//  Created by TaiTQ2 on 19/6/26.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+    
+}
